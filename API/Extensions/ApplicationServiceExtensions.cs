7e75909f10c3d55809f30d5ef7113921a4aef96e
amanda rice <aricericerice@gmail.com>
2021-10-07 17:03:43 -0600

    created repos

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.Helpers;
 using API.Interfaces;
 using API.Services;
 using Microsoft.EntityFrameworkCore;
@@ -14,13 +15,15 @@ namespace API.Extensions
     // Static means we don't need to create a new instance of this class to use it
     public static class ApplicationServiceExtensions
     {
-        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
-        {
-            services.AddScoped<ITokenService, TokenService>();
-            services.AddDbContext<DataContext>(options =>
-        {
-            options.UseSqlite(config.GetConnectionString("DefaultConnection"));
-        });
+    public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
+    {
+      services.AddScoped<ITokenService, TokenService>();
+      services.AddScoped<IUserRepository, UserRepository>();
+      services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
+      services.AddDbContext<DataContext>(options =>
+    {
+        options.UseSqlite(config.GetConnectionString("DefaultConnection"));
+    });
       return services;
     }
     }
--- API/Startup.cs
+++ API/Startup.cs
@@ -56,8 +56,8 @@ namespace API
         app.UseSwagger();
         app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
       }
-      app.UseMiddleware<ExceptionMiddleware>();
 
+      app.UseMiddleware<ExceptionMiddleware>();
       app.UseHttpsRedirection();
 
       app.UseRouting();
